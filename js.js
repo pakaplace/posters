abb077554d0815f854d558679266112e0af2c6e0
nlam511 <nathanlam@dhcp-v880-22-02729.1Xwireless.unc.edu>
2017-12-10 14:07:38 -0500

    masonry fix

--- js.js
+++ js.js
@@ -1,10 +1,29 @@
+const $container= null;
 
-var $rows = $('#table tr');
-$('#search').keyup(function() {
-    var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
-    
-    $rows.show().filter(function() {
-        var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
-        return !~text.indexOf(val);
-    }).hide();
+$(window).on('load' , function(){
+    $container= $('div.container').masonry({
+    columnWidth: '.picture',
+    itemSelector: '.picture',
+    percentPosition: true
 });
+});
+
+
+
+
+$('div.container').imagesLoaded( { background: true }, function() {
+    alert('images loaded')
+  console.log('#container background image loaded');
+});
+
+
+
+//var $rows = $('#table tr');
+//$('#search').keyup(function() {
+//    var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+//    
+//    $rows.show().filter(function() {
+//        var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+//        return !~text.indexOf(val);
+//    }).hide();
+//});
