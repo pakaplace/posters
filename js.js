e0a10f58a294ecc334a01a843539ca06f2cae2de
aleto <aleto@live.unc.edu>
2017-12-12 02:52:30 -0500

    JS for focusing
    Added enlarging focus and opacity
    Header padding

--- js.js
+++ js.js
@@ -11,12 +11,37 @@ $(window).on('load' , function(){
 $(document).ready(function() {
     $(".picture").click(function() {
         const $img = $(this);
-		$img.addClass('focus');
-	
-		$('div.container').masonry();
 		
-		})
-               })
+		if($img.hasClass('focus')) {
+			$img.removeClass('focus');
+			$('div.container > img').each(function (){
+			const $curr = $(this);
+			$curr.removeClass('notFocus');
+			});
+			
+		} else if ($img.hasClass('notFocus')){
+			
+			$('div.container > img').each(function (){
+			const $curr = $(this);
+			if($curr.hasClass('focus')){
+				$curr.removeClass('focus');
+				$curr.addClass('notFocus');
+				}
+			$img.removeClass('notFocus');
+			$img.addClass('focus');
+			});
+		} else {
+			$img.addClass('focus');
+			$('div.container > img').each(function (){
+			const $curr = $(this);
+			if($curr.hasClass('focus') === false){
+				$curr.addClass('notFocus');
+				}
+			});
+		}
+		$('div.container').masonry();
+		});
+    })
 
 
 $('div.container').imagesLoaded( { background: true }, function() {
