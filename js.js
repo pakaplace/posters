9512276f765580f7aaa3d994b13d655b31c03448
nlam511 <nathanlam@dhcp-v880-3-00820.1Xwireless.unc.edu>
2017-12-12 00:04:54 -0500

    js edit

--- js.js
+++ js.js
@@ -17,13 +17,3 @@ $('div.container').imagesLoaded( { background: true }, function() {
 });
 
 
-
-//var $rows = $('#table tr');
-//$('#search').keyup(function() {
-//    var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
-//    
-//    $rows.show().filter(function() {
-//        var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
-//        return !~text.indexOf(val);
-//    }).hide();
-//});
